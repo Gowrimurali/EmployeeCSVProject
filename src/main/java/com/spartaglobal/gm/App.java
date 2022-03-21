27c215dfcb7681447e1b254a50befb792812271e
MattWVile <matt.vile@yahoo.co.uk>
2022-03-21 15:56:36 +0000

    set up file util and file reader method

--- src/main/java/com/spartaglobal/gm/App.java
+++ src/main/java/com/spartaglobal/gm/App.java
@@ -1,5 +1,7 @@
 package com.spartaglobal.gm;
 
+import java.io.File;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        FileUtils.readFile("src/main/resources/EmployeeRecords.csv");
     }
 }
