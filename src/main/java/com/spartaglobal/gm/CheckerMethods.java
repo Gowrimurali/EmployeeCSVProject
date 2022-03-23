743ebac9501fde220eb148d9732b0863bebf255c
MattWVile <matt.vile@yahoo.co.uk>
2022-03-23 15:27:20 +0000

    Made a checker to check if gender is M or F and added to idchecker to ensure id is 6 digitd

--- src/main/java/com/spartaglobal/gm/CheckerMethods.java
+++ src/main/java/com/spartaglobal/gm/CheckerMethods.java
@@ -5,14 +5,24 @@ import static com.spartaglobal.gm.EmployeeDTO.employees;
 public class CheckerMethods {
 
     public boolean employeeIdChecker(EmployeeDTO employee ){
+        if (employee.getEmpID()<= 99999){
+            return false;
+        }
         for(int i = 0; i < employees.size(); i++){
             String values = employees.get(i);
             String[] splitValues = values.split(",");
             int employeeNum = Integer.parseInt(splitValues[0]);
             if(employeeNum == employee.getEmpID()){
-                return true;
+                return false;
             }
+        }
+        return true;
+    }
+    public boolean genderChecker(EmployeeDTO employee){
+        if (employee.getGender() == 'M' || employee.getGender() == 'F'){
+            return true;
         }
         return false;
     }
+
 }
