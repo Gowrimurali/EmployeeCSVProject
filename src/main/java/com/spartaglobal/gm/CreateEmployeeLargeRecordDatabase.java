6d7bd42805a4cfadc76485ef41c8eb0e45328b2c
Gowrimurali <GBabyamma@spartaglobal.com>
2022-03-24 14:36:37 +0000

    Adding Logs

--- src/main/java/com/spartaglobal/gm/CreateEmployeeLargeRecordDatabase.java
+++ src/main/java/com/spartaglobal/gm/CreateEmployeeLargeRecordDatabase.java
@@ -1,12 +1,27 @@
 package com.spartaglobal.gm;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class CreateEmployeeLargeRecordDatabase {
     public static void createLargeRecordTable(String[] largeEmployeeRecordData) {
         Connection connection = ConnectionManager.getConnection();
         EmployeeDAO employeeDAO = new EmployeeDAO(connection);
         employeeDAO.insertINTOEMployeeRecordsLarge(Integer.parseInt(largeEmployeeRecordData[0]), largeEmployeeRecordData[1], largeEmployeeRecordData[2], largeEmployeeRecordData[3], largeEmployeeRecordData[4], largeEmployeeRecordData[5], largeEmployeeRecordData[6], largeEmployeeRecordData[7], largeEmployeeRecordData[8], Integer.parseInt(largeEmployeeRecordData[9]));
+        ConnectionManager.closeConnection();
+    }
+
+    public static void deleteFromEmployeeLargeRecords(){
+        Connection connection = ConnectionManager.getConnection();
+        EmployeeDAO employeeDAO = new EmployeeDAO(connection);
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement("DELETE FROM `employeerecords`.`employeerecordslarge`");
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         ConnectionManager.closeConnection();
     }
 }
