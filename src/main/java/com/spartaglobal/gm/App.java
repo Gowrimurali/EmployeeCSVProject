fea702e91afce9a0643aa4b4fe6bb2d6e42a71da
MattWVile <matt.vile@yahoo.co.uk>
2022-03-24 11:44:09 +0000

    corrupted and uncorrupted lists working correctly

--- src/main/java/com/spartaglobal/gm/App.java
+++ src/main/java/com/spartaglobal/gm/App.java
@@ -5,6 +5,7 @@ public class App
     public static void main(String[] args) {
         FileUtils.readFile("src/main/resources/EmployeeRecords.csv");
         Display.displayFiles();
+        
     }
 }
 
