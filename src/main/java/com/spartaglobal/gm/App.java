717413dc248befd87463ee2b5c54edfe594a4fa3
Gowrimurali <GBabyamma@spartaglobal.com>
2022-03-24 09:06:46 +0000

    Creating Employee large record database

--- src/main/java/com/spartaglobal/gm/App.java
+++ src/main/java/com/spartaglobal/gm/App.java
@@ -1,9 +1,17 @@
 package com.spartaglobal.gm;
 
-public class App 
+import java.sql.Connection;
+
+public class App
 {
     public static void main(String[] args) {
-        FileUtils.readFile("src/main/resources/EmployeeRecords.csv");
+//        FileUtils.readFile("src/main/resources/EmployeeRecords.csv");
+        double startTime = System.nanoTime();
+        FileReadForLargeRecord.readFile("src/main/resources/EmployeeRecordsLarge.csv");
+        double stopTime = System.nanoTime();
+        System.out.println(startTime);
+        System.out.println(stopTime);
+        System.out.println("Total time is: " + (stopTime-startTime));
     }
 }
 
